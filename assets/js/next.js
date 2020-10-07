4c04c3daf0e1fbe27d7527f3e2706b53e56c7ae5
Fasemore A. Mandela <27198406+fasemoreakinyemi@users.noreply.github.com>
2020-10-07 15:55:15 +0200

    Update next.js

--- assets/js/next.js
+++ assets/js/next.js
@@ -9,7 +9,7 @@ swap()
   
 function swap() {
     var items = document.querySelectorAll(".row"),
-    var index = document.querySelector('.current').index
+    var index = document.querySelector('.current').dataset.index
     var nextIndex;
     if (index == items.length) {
         nextIndex = 1 }
