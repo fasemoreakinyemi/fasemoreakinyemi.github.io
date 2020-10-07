173d91af827e64e06925a0494ed85577ae5f9a11
Fasemore A. Mandela <27198406+fasemoreakinyemi@users.noreply.github.com>
2020-10-07 16:56:40 +0200

    Update next.js

--- assets/js/next.js
+++ assets/js/next.js
@@ -12,10 +12,11 @@ function swap() {
     var index = document.querySelector('.current').dataset.index;
     var nextIndex = index + 1
     alert(nextIndex)
-    if (index == items.length) {
-        nextIndex = 1 } 
+   
    var current = items[index-1]
-   var nextRow = items[nextIndex-1]
+   var nextRow = items[index]
+    if (index == items.length){
+    nextRow = items[0]} 
    nextRow.classList.remove('hidden');
    current.classList.add('hidden');
    current.classList.remove('current');
